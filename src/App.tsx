721490a5e6a8189bdb7a770c8b575de9e3ee2ebc
BBSoman <harshumeshshah23@hotmail.com>
2026-06-15 11:35:29 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from '../context/LanguageContext';
+import { LanguageProvider } from './context/LanguageContext';
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 import CategoryDetailPage from './pages/CategoryDetailPage';
