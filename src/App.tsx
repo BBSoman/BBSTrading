e059d1066f7946ff7e6d541b96592088cecc2a0c
BBSoman <harshumeshshah23@hotmail.com>
2026-06-15 10:44:02 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from './context/LanguageContext';
+import { LanguageProvider } from '../context/LanguageContext';
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 import CategoryDetailPage from './pages/CategoryDetailPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import ContactPage from './pages/ContactPage';
 import SubProductDetailPage from './pages/SubProductDetailPage';
+
 
 function App() {
   return (
